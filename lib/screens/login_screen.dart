dd73dec1d907e6076138f0456f505e13932a2697
André Campos <andrecampos.ds@gmail.com>
2019-11-18 13:56:00 -0300

    Added validator on TextFormField

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -6,6 +6,12 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final _formKey = GlobalKey<FormState>();
+
+  final usernameRegex = RegExp(r"^([A-z][A-z0-9\.\_]{3,})$", multiLine: true);
+  final passwordRegex =
+      RegExp(r"^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$\%\^&])(?=.{6,})");
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,28 +24,36 @@ class _LoginScreenState extends State<LoginScreen> {
   }
 
   Widget _createBody(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(16),
-      child: ListView(
-        children: <Widget>[
-          _createTextField(
-            context: context,
-            label: "Usuário",
-            hint: "Digite seu usuário",
-            keyboardType: TextInputType.emailAddress,
-            textCapitalization: TextCapitalization.none,
-          ),
-          SizedBox(height: 8),
-          _createTextField(
-            context: context,
-            label: "Senha",
-            hint: "Digite sua senha",
-            textCapitalization: TextCapitalization.none,
-            obscureText: true,
-          ),
-          SizedBox(height: 36),
-          _createLoginButton(context),
-        ],
+    return Form(
+      key: _formKey,
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        child: ListView(
+          children: <Widget>[
+            _createTextField(
+              context: context,
+              label: "Usuário",
+              hint: "Digite seu usuário",
+              keyboardType: TextInputType.emailAddress,
+              textCapitalization: TextCapitalization.none,
+              validator: _validateUser,
+            ),
+            SizedBox(height: 8),
+            _createTextField(
+              context: context,
+              label: "Senha",
+              hint: "Digite sua senha",
+              textCapitalization: TextCapitalization.none,
+              obscureText: true,
+              validator: _validatePassword,
+            ),
+            SizedBox(height: 36),
+            _createButton(
+              context: context,
+              onPressed: _onClickLoginButton,
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -50,18 +64,21 @@ class _LoginScreenState extends State<LoginScreen> {
       String hint,
       bool obscureText = false,
       TextCapitalization textCapitalization = TextCapitalization.sentences,
-      TextInputType keyboardType = TextInputType.text}) {
+      TextInputType keyboardType = TextInputType.text,
+      FormFieldValidator<String> validator}) {
     return TextFormField(
       decoration: InputDecoration(
-          labelText: label,
-          labelStyle: TextStyle(
-            color: Colors.grey,
-            fontSize: 18,
-          ),
-          hintText: hint,
-          hintStyle: TextStyle(
-            fontSize: 18,
-          )),
+        labelText: label,
+        labelStyle: TextStyle(
+          color: Colors.grey,
+          fontSize: 18,
+        ),
+        hintText: hint,
+        hintStyle: TextStyle(
+          fontSize: 18,
+        ),
+        errorMaxLines: 3,
+      ),
       keyboardType: keyboardType,
       obscureText: obscureText,
       style: TextStyle(
@@ -69,10 +86,12 @@ class _LoginScreenState extends State<LoginScreen> {
         fontSize: 18,
       ),
       textCapitalization: textCapitalization,
+      validator: validator,
     );
   }
 
-  Widget _createLoginButton(BuildContext context) {
+  Widget _createButton(
+      {@required BuildContext context, @required VoidCallback onPressed}) {
     return SizedBox(
       height: 48,
       child: RaisedButton(
@@ -84,8 +103,32 @@ class _LoginScreenState extends State<LoginScreen> {
             fontSize: 20,
           ),
         ),
-        onPressed: () {},
+        onPressed: onPressed,
       ),
     );
+  }
+
+  void _onClickLoginButton() {
+    _formKey.currentState.validate();
+  }
+
+  String _validateUser(String username) {
+    if (username.length < 4) {
+      return "O usuário deve conter no mínimo 4 caracteres.";
+    }
+
+    if (!usernameRegex.hasMatch(username)) {
+      return "O usuário deve conter somente letras, números, \".\" ou \"_\"";
+    }
+
+    return null;
+  }
+
+  String _validatePassword(String password) {
+    if (!passwordRegex.hasMatch(password)) {
+      return "A senha deve possuir ao menos seis caracteres, sendo uma letra maiúscula, uma minúscula, um número e um caractere especial";
+    }
+
+    return null;
   }
 }
